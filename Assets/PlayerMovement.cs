5bd5d9d27cf52c33e5702f3e57466bd91cf0260a
Hilda <eskarina@eskarina.ca>
2021-04-23 22:54:40 -0700

    Added laser weapon prototype

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -11,6 +11,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float rollSpeed = 1.0f;
     [SerializeField] bool invertY = false;
 
+    [SerializeField] GameObject laserPrefab;
+
+    [SerializeField] float laserFireRate = 1.0f;
+
+    float laserCooldown;
+
     Rigidbody body;
 
     // Start is called before the first frame update
@@ -30,6 +36,16 @@ public class PlayerMovement : MonoBehaviour
     {
         body.AddRelativeTorque(GetRotation(), ForceMode.VelocityChange);
         body.AddRelativeForce(GetDirection() * moveSpeed, ForceMode.VelocityChange);
+    
+        if(laserCooldown >= 0)
+        {
+            laserCooldown -= Time.fixedDeltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            FireLaser();
+        }
     }
 
     Vector3 GetDirection()
@@ -41,7 +57,7 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.A)) dir += Vector3.left;
         if (Input.GetKey(KeyCode.D)) dir += Vector3.right;
         if (Input.GetKey(KeyCode.LeftControl)) dir += Vector3.down;
-        if ((Input.GetKey(KeyCode.Space)) || (Input.GetKey(KeyCode.LeftShift))) dir += Vector3.up;
+        if (Input.GetKey(KeyCode.LeftShift)) dir += Vector3.up;
         return dir;
     }
 
@@ -53,5 +69,15 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.Q)) roll += 1;
         if (Input.GetKey(KeyCode.E)) roll -= 1;
         return new Vector3(pitch * turnSpeed, yaw * turnSpeed, roll * rollSpeed);
+    }
+
+    private void FireLaser()
+    {
+        if (laserCooldown <= 0)
+        {
+            GameObject newLaser = Instantiate(laserPrefab, gameObject.transform.position - gameObject.transform.up, gameObject.transform.rotation);
+            newLaser.GetComponent<Rigidbody>().velocity = gameObject.transform.forward * 40.0f;
+            laserCooldown = laserFireRate;
+        }
     }
 }
