02c337159962872c0262e5f32737a8787bc4a1e1
Hilda <eskarina@eskarina.ca>
2021-04-26 05:13:24 -0700

    gharpoon agrabjagrargen

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -270,7 +270,7 @@ public class PlayerMovement : MonoBehaviour
                 harpoonRopeLength = harpoonDistance;
             }
 
-            if (harpoonRopeLength < 2.0f || harpoonScript.stuckInTerrain)
+            if (harpoonRopeLength < 5.0f || harpoonScript.stuckInTerrain)
             {
                 harpoonScript.DetachHarpoon();
             }
